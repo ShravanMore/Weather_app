c4dbfcf3c492eefd0652adbf6383aa80b80a44d4
Shravan More <shravanmore125@gmail.com>
2024-06-29 19:43:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Weather from './Component/Weather'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Weather /> {/* Use the Weather component */}
+    </div>
+  );
+}
+
+export default App;
